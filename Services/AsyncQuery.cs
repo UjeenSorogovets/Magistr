1dbfaf766fc85eaf5cbbd456937d42b344082880
UjeenSorogovets <84143490+UjeenSorogovets@users.noreply.github.com>
2022-03-29 02:13:33 +0300

    add services

---
+++ Services/AsyncQuery.cs
@@ -0,0 +1,50 @@
+﻿namespace WebApplication.Services
+{
+    public class AsyncQuery
+    {
+        private readonly CommonService commonService;
+        private int asyncCount = 2;
+        public AsyncQuery()
+        {
+            commonService = new CommonService();
+        }
+
+        async Task AsyncTask(int n, IEnumerable<int> numbers)
+        {
+            await Task.Run(() =>
+            {
+                var startTime = System.Diagnostics.Stopwatch.StartNew();
+                commonService.Primes(numbers);
+                startTime.Stop();
+                Console.WriteLine($"{n} Async - {startTime.ElapsedMilliseconds}ms");
+            });
+        }
+
+        public async Task Run(IEnumerable<int> numbers)
+        {
+            for (int i = 0; i < asyncCount; i++)
+            {
+                await AsyncTask(i, numbers);
+            }
+        }
+
+        public async Task OneAsyncQuery(IEnumerable<int> numbers)
+        {
+            var startTime = System.Diagnostics.Stopwatch.StartNew();
+            Console.WriteLine("Start ASYNC Single results");
+            await Run(numbers);
+            startTime.Stop();
+            Console.WriteLine($"Final Single Async - {startTime.ElapsedMilliseconds}ms" + Environment.NewLine);
+        }
+
+        public async Task WhenAllAsyncQuery(IEnumerable<int> numbers)
+        {
+            Console.WriteLine("Start WhenAll results");
+            Console.WriteLine("Start WhenAll results");
+            var startTime = System.Diagnostics.Stopwatch.StartNew();
+            await Task.WhenAll(Run(numbers), Run(numbers));
+            startTime.Stop();
+            Console.WriteLine($"Final WhenAll result \nAsync - {startTime.ElapsedMilliseconds}ms" + Environment.NewLine);
+        }
+    }
+}
---
+++ Services/ParallelQueryService.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication.Services
+{
+    public class ParallelQueryService
+    {
+        private readonly CommonService commonService;
+        public ParallelQueryService()
+        {
+            commonService = new CommonService();
+        }
+
+        public void Run(IEnumerable<int> numbers)
+        {
+            var startTime = System.Diagnostics.Stopwatch.StartNew();
+            Console.WriteLine("Start Parallel");
+            var parallelQuery =
+               from n in numbers.AsParallel()
+               where Enumerable.Range(2, (int)Math.Sqrt(n)).All(i => n % i > 0)
+               select n;
+            int[] primes = parallelQuery.ToArray();
+            startTime.Stop();
+            Console.WriteLine($"Parallel - {startTime.ElapsedMilliseconds}ms" + Environment.NewLine);
+        }
+    }
+}
