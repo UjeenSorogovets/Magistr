3a3a7d568da9ed1e27db022273d51d99af95c9fe
UjeenSorogovets <84143490+UjeenSorogovets@users.noreply.github.com>
2022-03-29 03:12:49 +0300

    add controller

---
+++ Controllers/SimpleQueryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Services;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleQueryController : ControllerBase
+    {
+        private readonly ILogger<QueryController> _logger;
+        private static readonly IEnumerable<int> numbers = Enumerable.Range(3, 1000000);
+        private readonly SimpleQueryService simpleQuery = new();
+        private readonly int[] degrees = { 10, 100, 1000, 10000, 100_000, 500_000, 1_000_000 };
+        public SimpleQueryController(ILogger<QueryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("RunSimple")]
+        public void RunSimple()
+        {
+            simpleQuery.RunMultiple(degrees);
+        }
+
+    }
+}
